be740820810b6967931d29aacfccbed46b5a656f
Shaikat1952 <asmshaikat1952@gmail.com>
2021-06-04 04:03:35 -0700

    6/4/2021

---
+++ Concatanent.php
@@ -0,0 +1,5 @@
+<?php
+$fname='Abu Shadat';$lname='Shaikat';
+echo"$fname ","$lname <br>";
+echo"$fname "."$lname";
+?>
---
+++ Foreach.php
@@ -0,0 +1,7 @@
+<?php
+//$age=['shaikat'=>20,'Ali'=>20];
+$age=[10,20,30,40];
+foreach($age as $data){
+    echo $data ."<br>";
+}
+?>
