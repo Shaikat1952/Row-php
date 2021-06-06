ba7cb9926e94eac7c314101348eaea7aaa7a2a70
Abu Shadat Shaikat <asmshaikat1952@gmail.com>
2021-06-06 08:19:56 -0700

    4 files added

---
+++ ArraySlice.php
@@ -0,0 +1,7 @@
+<?php
+$name=['shaikat','shadat','abu'];
+$collect=array_slice($name,0,1);
+echo "<pre>";
+print_r($collect);
+echo "</pre>";
+?>
---
+++ PushPop.php
@@ -0,0 +1,8 @@
+<?php
+$str=["me","us","you","them"];
+array_pop($str);
+array_push($str,"her");
+echo "<pre>";
+print_r($str);
+echo "</pre>";
+?>
