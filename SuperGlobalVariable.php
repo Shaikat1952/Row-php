d95cf837d16982b2885f27e9aa530776f23949f7
Abu Shadat Shaikat <asmshaikat1952@gmail.com>
2021-06-10 09:19:28 -0700

    Super global variable added

---
+++ SuperGlobalVariable.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fill the form</title>
+</head>
+<body>
+    <form action="SuperGlobalVDatabase.php" method="POST">
+    Name: <input type="text" name="Name"><br><br>
+    Age : <input type="text" name="Age"><br><br>
+    ID  : <input type="text" name="ID"><br><br>
+    <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
